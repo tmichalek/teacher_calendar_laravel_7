4e6d32db86a741d6605ec43506ad9c6ba89d2230
Tomasz Michałek <tomasz.michalek@onet.pl>
2021-02-21 14:36:41 +0100

    Add role, role_user tables and delete userKind column from teachers and students tables

---
+++ database/migrations/2021_02_20_190801_create_role_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoleUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role_user', function (Blueprint $table) {
+            $table->id('id_role_user');
+            $table->foreignId('id_role');
+            $table->foreign('id_role')->references('id_role')->on('role');
+            $table->foreignId('id_user');
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('role_user');
+    }
+}
